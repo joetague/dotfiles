11bad5f3ed20f48feb652bbe5d1b39015704aefa
Joe Tague <joetague@users.noreply.github.com>
2024-09-20 21:36:23 +0100

    fix: key delay and repeat for macos

--- scripts/macos_tweaks.sh
+++ scripts/macos_tweaks.sh
@@ -1,6 +1,10 @@
-defaults write com.apple.finder DisableAllAnimations -bool true
-defaults write com.apple.dock launchanim -bool false
-defaults write -g NSWindowResizeTime -float 0.001
-defaults write com.apple.dock expose-animation-duration -float 0.1
-defaults write com.apple.dock autohide-time-modifier -float 0
-defaults write com.apple.mail DisableReplyAnimations -bool true
+#defaults write com.apple.finder DisableAllAnimations -bool true
+#defaults write com.apple.dock launchanim -bool false
+#defaults write -g NSWindowResizeTime -float 0.001
+#defaults write com.apple.dock expose-animation-duration -float 0.1
+#defaults write com.apple.dock autohide-time-modifier -float 0
+#defaults write com.apple.mail DisableReplyAnimations -bool true
+
+# https://gist.github.com/hofmannsven/ff21749b0e6afc50da458bebbd9989c5
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1         # normal minimum is 2 (30 ms)
