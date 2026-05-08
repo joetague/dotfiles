531f490841a597385379c42ca2c8eafadeb78e4f
Joe Tague <joetague@users.noreply.github.com>
2026-05-08 20:42:52 +0100

    shell-pop api change not in spacemacs yet, apply workarond

--- spacemacs/private/personal/config.el
+++ spacemacs/private/personal/config.el
@@ -10,3 +10,9 @@
 ;; by default ~/.authinfo will be used
 ;; and write a token in unencrypted format
 (setopt auth-sources '("~/.authinfo.gpg"))
+
+;; Shim: upstream shell-pop dropped `shell-pop-last-shell-buffer-name', but
+;; Spacemacs's shell layer still references it (layers/+tools/shell/funcs.el).
+(with-eval-after-load 'shell-pop
+  (unless (boundp 'shell-pop-last-shell-buffer-name)
+    (defvar shell-pop-last-shell-buffer-name "")))
