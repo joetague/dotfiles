455ff9a45bbaa2650538b2b42a9892d7aaed7027
Joe Tague <joetague@users.noreply.github.com>
2026-08-06 19:04:21 +0100

    remove empty files

--- spacemacs/private/personal-shell/layers.el
+++
@@ -1,7 +0,0 @@
-;;; layers.el --- personal-shell layer dependencies file for Spacemacs. -*- lexical-binding: t -*-
-
-;;; Code:
-
-(configuration-layer/declare-layer-dependencies '(shell))
-
-;;; layers.el ends here
